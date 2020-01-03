4a795733ab7b7357cdc7303811c8c313d57644d5
Jose Alvarez <Jose.Alvarez@Falconeye.local>
2020-01-03 17:32:37 -0400

    Buscador hecho

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="col-12 je-img">
+        <div className="container">
+          <form class="">
+            <div style={{height: "450px"}} class="row d-flex align-content-center flex-wrap">
+              <div class="col-sm-12 col-md-8 p-0 m-1 shadow">
+                <input type="text" class="form-control form-control-lg" placeholder="Buscar imagen..."/>
+              </div>
+              <div class="col-sm-12 col-md-3 p-0 m-1 shadow">
+                <button type="submit" className="btn btn-secondary btn-lg btn-block">Buscar <span uk-icon="search"></span></button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
